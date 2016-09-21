c4470403b20d6bb89deeac487ff5292cd5c5def1
coco11563 <coco11563@yeah.net>
2016-09-21 12:15:54 +0800

    建立文件目录 导入相关jar包 资源图片xml导入

---
+++ app/src/androidTest/java/pub/sha0w/coco/findlecture/ApplicationTest.java
@@ -0,0 +1,13 @@
+package pub.sha0w.coco.findlecture;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
